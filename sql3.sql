b0e71e67ccb1e8b990700a0e8c84fb5aed6868b6
AbhayaV2001 <abhayav.cs18@bmsce.ac.in>
2020-05-02 11:13:18 +0530

    Supplier database

--- sql3.sql
+++ sql3.sql
@@ -1,42 +1,68 @@
-show databases;
-create database week3;
-show databases;
-use abhaya;
-create table Supplier(sid int,sname varchar(20),city varchar(20),PRIMARY KEY(sid));
-create table Parts(pid int,pname varchar(20),color varchar(20),PRIMARY KEY(pid));
-create table Catalog(sid int,pid int,cost double,PRIMARY KEY(sid,pid),foreign key(sid) references Supplier(sid),foreign key(pid) references Parts(pid));
-insert into Supplier
-Values (10001,'Acme Widget','Bangalore'),
-(10002,'Johns','Kolkata'),
-(10003,'Vimal','Mumbai'),
-(10004,'Reliance','Delhi');
-insert into Parts
-Values (20001,'Book','Red'),
-(20002,'Pen','Red'),
-(20003,'Pencil','Green'),
-(20004,'Mobile','Green'),
-(20005,'Charger','Black');
-insert into Catalog
-Values (10001,20001,10),
-(10001,20002,10),
-(10001,20003,10),
-(10001,20004,30),
-(10001,20005,10),
-(10002,20001,10),
-(10002,20002,10),
-(10003,20003,20),
-(10004,20003,40);
+create database SupplierDB;
+use SupplierDB;
+create table Supplier(
+sid int,
+sname varchar(30),
+city varchar(30),
+primary key(sid));
+desc Supplier;
+create table Parts(
+pid int,
+pname varchar(30),
+color varchar(30),
+primary key(pid));
+create table Catalog(
+sid int,
+pid int,
+cost int,
+primary key(sid,pid),
+foreign key (sid) references Supplier(sid),
+foreign key (pid) references Parts(pid));
+insert into Supplier values(10001,'Acme Widget','Bangalore'),(10002,'Johns','Kolkata'),(10003,'Vimal','Mumbai'),(10004,'Reliance','Delhi');
+insert into Parts values(20001,'Book','Red'),(20002,'Pen','Red'),(20003,'pencil','Green'),(20004,'Mobile','Green'),(20005,'Charger','Black');
+insert into Catalog values(10001,20001,10),(10001,20002,10),(10001,20003,30),(10001,20004,10),(10001,20005,10),(10002,20001,10),(10002,20002,20),(10003,20003,30),(10004,20003,40);
+select * from Supplier;
+select * from Parts;
+select * from Catalog;
 
-select * from Supplier; 
-select * from Parts; 
-select * from Catalog; 
+select distinct pname 
+from Parts, Catalog
+where Parts.pid=Catalog.pid;
 
-select distinct(sid) SID from Catalog,Parts where Catalog.pid=Parts.pid and Parts.color='Red' or Parts.color='Green';
-select distinct(Catalog.sid) SID from Catalog,Parts,Supplier where Catalog.pid=Parts.pid and Catalog.sid=Supplier.sid and (Parts.color='Red' or Supplier.city='Bangalore');
-select  distinct c1.sid,c2.sid from Catalog c1,Catalog c2 where c1.sid!=c2.sid and (c1.pid=c2.pid and c1.cost>c2.cost);
-select distinct(sid) SID from Catalog,Parts where Catalog.pid=Parts.pid and Parts.color='Red';
-select sname,pname 
-from Supplier,Parts,Catalog C
-where  Parts.pid=C.pid and Supplier.sid=C.sid and C.cost=(select max(C1.cost)
-																	from Catalog C1
-																	where Parts.pid=C1.pid);
+select sname 
+from Supplier
+where not exists( select pid from Parts
+					except
+                    select distinct pid 
+                    from Catalog
+                    where Catalog.sid=Supplier.sid);
+
+select sname 
+from Supplier
+where not exists( select pid from Parts where color='Red'
+					except
+                    select distinct Catalog.pid 
+                    from Catalog , Parts
+                    where Parts.pid=Catalog.pid and Parts.color='Red' and  Catalog.sid=Supplier.sid);
+                    
+
+select pname
+from Parts,Catalog,Supplier
+where Catalog.pid=Parts.pid and Catalog.sid=Supplier.sid 
+and Supplier.sname='Acme Widget' and Catalog.pid not in (select c.pid from Catalog c ,Supplier s
+							where s.sid=c.sid and s.sname<>'Acme Widget');
+
+
+select sid
+from Catalog c
+where c.cost>( select avg(c1.cost)
+		from Catalog c1
+		where c.pid=c1.pid);
+                
+                
+select p.pid, s.sname
+from Parts p, Supplier s, Catalog c
+where p.pid=c.pid and s.sid=c.sid
+and c.cost=(select max(c1.cost)
+	    from Catalog c1
+            where c1.pid=c.pid);
